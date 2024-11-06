c48b2b11b13ef9f1bc3a09b3e66ae8b3a836eb0c
Drusha02 <hanz.dumapit54@gmail.com>
2024-11-06 14:45:29 +0800

    - db

--- database/migrations/2024_11_06_052835_space_pictures_table.php
+++ database/migrations/2024_11_06_052835_space_pictures_table.php
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('spaces');
+        Schema::dropIfExists('space_pictures');
     }
 };
