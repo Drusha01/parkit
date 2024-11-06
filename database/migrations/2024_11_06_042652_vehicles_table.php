3729c2e9ac9331052cf5ae098d6523ed5097d0c5
Drusha02 <hanz.dumapit54@gmail.com>
2024-11-06 12:59:50 +0800

    - db

--- database/migrations/2024_11_06_042652_vehicles_table.php
+++ database/migrations/2024_11_06_042652_vehicles_table.php
@@ -14,7 +14,22 @@ return new class extends Migration
         DB::statement(("
         CREATE TABLE vehicles(
             id INT PRIMARY KEY AUTO_INCREMENT,
-            name VARCHAR(255) NOT NULL, 
+            user_id INT NOT NULL,
+            vehicle_type_id INT NOT NULL,
+            cr_file_number VARCHAR(255) NOT NULL,
+            cr_plate_number VARCHAR(20),
+            cr_no VARCHAR(50) ,
+            cr_vehicle_owner VARCHAR(300),
+            cr_picture VARCHAR(50) NOT NULL,
+            or_picture VARCHAR(50) NOT NULL,
+            or_expiration_date DATE NOT NULL,
+            or_color VARCHAR(50) NOT NULL,
+            front_size_picture VARCHAR(50) NOT NULL,
+            back_side_picture VARCHAR(50) NOT NULL,
+            left_side_picture VARCHAR(50) NOT NULL,
+            right_side_picture VARCHAR(50) NOT NULL,
+
+  
             date_created DATETIME DEFAULT CURRENT_TIMESTAMP,
             date_updated DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
         );"));
---
+++ database/migrations/2024_11_06_045542_spaces_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+           DB::statement(("
+        CREATE TABLE spaces(
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            user_id INT NOT NULL,
+            area_m2 DOUBLE NOT NULL,
+            location_long DOUBLE NOT NULL,
+            location_lat DOUBLE NOT NULL,
+            date_created DATETIME DEFAULT CURRENT_TIMESTAMP,
+            date_updated DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+        );"));
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('spaces');
+    }
+};
