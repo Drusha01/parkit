06b68ba121f8b830b556d02fae3fb59996d14d6d
Drusha02 <hanz.dumapit53@gmail.com>
2025-04-22 02:31:29 +0800

    admin nav bug fix

--- resources/js/Components/SideNavigation/AdminNav.jsx
+++ resources/js/Components/SideNavigation/AdminNav.jsx
@@ -65,7 +65,7 @@ export const AdminNav = ({props}) => {
                         </Link>
                     </li>
                     <li className={url === "/admin/notifications" ? "w-[w-full] h-12 rounded-md text-white bg-blue-900 hover:bg-blue-900" : "w-[w-full] h-12 rounded-md text-white hover:bg-blue-900"}>
-                        <Link href="admin/notifications" className="flex h-full gap-2 mx-3">
+                        <Link href="/admin/notifications" className="flex h-full gap-2 mx-3">
                             <svg fill="#fff" width="32" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M10,21h4a2,2,0,0,1-4,0ZM3.076,18.383a1,1,0,0,1,.217-1.09L5,15.586V10a7.006,7.006,0,0,1,6-6.92V2a1,1,0,0,1,2,0V3.08A7.006,7.006,0,0,1,19,10v5.586l1.707,1.707A1,1,0,0,1,20,19H4A1,1,0,0,1,3.076,18.383ZM6.414,17H17.586l-.293-.293A1,1,0,0,1,17,16V10A5,5,0,0,0,7,10v6a1,1,0,0,1-.293.707Z"></path></g></svg>
                             <div className={props== true ? "mt-3" : "mt-3 hidden"}>
                                 Notifications
